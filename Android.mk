a9a427b088973d84552be74183a6d936c3f3aa9d
CyberJalagam <rbinternationalnetwork@gmail.com>
2020-12-24 10:37:56 +0530

    CPH1859: import full C.50 blobs

--- Android.mk
+++ Android.mk
@@ -22,21 +22,19 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := SoterService
 LOCAL_MODULE_OWNER := oppo
 LOCAL_SRC_FILES := proprietary/app/SoterService/SoterService.apk
+ifneq ($(filter CPH1859,$(TARGET_DEVICE)),)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ImsService
+LOCAL_MODULE_OWNER := oppo
+LOCAL_SRC_FILES := proprietary/priv-app/ImsService/ImsService.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
+LOCAL_PRIVILEGED_MODULE := true
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := vendor.qti.hardware.soter-V1.0-java
-LOCAL_MODULE_OWNER := oppo
-LOCAL_SRC_FILES := proprietary/framework/vendor.qti.hardware.soter-V1.0-java.jar
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := JAVA_LIBRARIES
-LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
 endif
